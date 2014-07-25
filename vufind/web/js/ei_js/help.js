a1f528ede6075b7792e280db0eb63e154bf80d4f
Mark Duffy <markaduffy@gmail.com>
2014-07-25 10:12:26 -0400

    side facets and centre header tool tips

--- vufind/web/js/ei_js/help.js
+++ vufind/web/js/ei_js/help.js
@@ -24,4 +24,88 @@ $( document ).ready(function() {
 	    }
 	})
 
+	$('.qtip-available_at').qtip({ // Grab some elements to apply the tooltip to
+	    content: {
+	        text: 'Click on a <span class="bold">Location</span> below to limit your search results to the holdings of the selected library.'
+	    },
+	    hide: {
+	        delay: 2000
+	    },
+	    style: {
+	        classes: 'qtip-rounded'
+	    }
+	})
+
+	$('.qtip-format').qtip({ // Grab some elements to apply the tooltip to
+	    content: {
+	        text: 'To limit your search by <span class="bold">Material Type(s)</span>, click on those you wish to include.  For example, DVD and Book on CD.  Click See All to view all available material types.'
+	    },
+	    hide: {
+	        delay: 2000
+	    },
+	    style: {
+	        classes: 'qtip-rounded'
+	    }
+	})
+
+	$('.qtip-target_audience_full').qtip({ // Grab some elements to apply the tooltip to
+	    content: {
+	        text: 'To limit your search by <span class="bold">Age Group</span>, click on one of the four options below.'
+	    },
+	    hide: {
+	        delay: 2000
+	    },
+	    style: {
+	        classes: 'qtip-rounded'
+	    }
+	})
+
+	$('.qtip-literary_form_full').qtip({ // Grab some elements to apply the tooltip to
+	    content: {
+	        text: 'To limit your search by <span class="bold">Literary Form</span>, click on an option below.'
+	    },
+	    hide: {
+	        delay: 2000
+	    },
+	    style: {
+	        classes: 'qtip-rounded'
+	    }
+	})
+
+	$('.qtip-topic_facet').qtip({ // Grab some elements to apply the tooltip to
+	    content: {
+	        text: 'To limit your search by <span class="bold">Subject</span>, click on an option below.'
+	    },
+	    hide: {
+	        delay: 2000
+	    },
+	    style: {
+	        classes: 'qtip-rounded'
+	    }
+	})
+
+	$('.qtip-authorStr').qtip({ // Grab some elements to apply the tooltip to
+	    content: {
+	        text: 'To limit your search by <span class="bold">Author</span>, click on an author’s name in the list below.'
+	    },
+	    hide: {
+	        delay: 2000
+	    },
+	    style: {
+	        classes: 'qtip-rounded'
+	    }
+	})
+
+	$('.qtip-language').qtip({ // Grab some elements to apply the tooltip to
+	    content: {
+	        text: 'To limit your search by <span class="bold">Language</span>, click on an available language option in the list below.'
+	    },
+	    hide: {
+	        delay: 2000
+	    },
+	    style: {
+	        classes: 'qtip-rounded'
+	    }
+	})
+
 });
